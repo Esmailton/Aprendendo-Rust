bc69be2be6c08fff69f583f4e0e7c6f89652d547
Esmailton Silva <buga_sg12@hotmail.com>
2022-07-26 22:06:52 -0300

    *Implementing calculator 	-This is a calculator implemented through Rust's basic 	 features with learning intuition.

--- Rust Basic/simple_calculator/src/main.rs
+++ Rust Basic/simple_calculator/src/main.rs
@@ -1,3 +1,47 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let signals_aritimetic = ['+', '-', '/', '*'];
+
+    println!("Enter your operation: ");
+
+    let mut operation = String::new();
+
+    io::stdin()
+        .read_line(&mut operation)
+        .expect("Failed to get operation");
+
+    for signal in signals_aritimetic.into_iter() {
+        if let Some(_result) = operation.find(signal) {
+
+            match signal {
+                '-' => {
+                    let values: Vec<&str> = operation.split(signal).collect();
+                    let f = values[0].trim().parse::<f32>().unwrap()
+                        - values[1].trim().parse::<f32>().unwrap();
+                    println!("The result is ->: {}", f);
+                }
+                '+' => {
+                    let values: Vec<&str> = operation.split(signal).collect();
+                    let f = values[0].trim().parse::<f32>().unwrap()
+                        + values[1].trim().parse::<f32>().unwrap();
+                    println!("The result is ->: {}", f);
+                }
+                '*' => {
+                    let values: Vec<&str> = operation.split(signal).collect();
+                    let f = values[0].trim().parse::<f32>().unwrap()
+                        * values[1].trim().parse::<f32>().unwrap();
+                    println!("The result is ->: {}", f);
+                }
+                '/' => {
+                    let values: Vec<&str> = operation.split(signal).collect();
+                    let f = values[0].trim().parse::<f32>().unwrap()
+                        / values[1].trim().parse::<f32>().unwrap();
+                    println!("The result is ->: {}", f);
+                }
+                _ => {
+                    println!("can't understand the operation");
+                }
+            };
+        }
+    }
 }
